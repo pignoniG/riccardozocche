7236f7d7a26fec64b618a97b6b3ced69ccd4e9d0
giovanni <pignoni.g@gmail.com>
2024-05-27 10:15:40 +0200

    init curved buttons

--- footer.php
+++ footer.php
@@ -11,8 +11,21 @@
 
 ?>
 
+<hr class="separator">
+
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+
+
+<svg height="200" width="350" xmlns="http://www.w3.org/2000/svg">
+  <path id="lineAC" d="M 10.729922,84.666667 V 59.785495 h 0.01654 c -0.0082,-0.108146 -0.01596,-0.216323 -0.02326,-0.324528 -4e-5,-23.296734 18.885704,-42.182478 42.182438,-42.182438 h 5.1e-5 c 23.296734,-4e-5 42.182481,18.885704 42.182441,42.182438 -0.002,0.108186 -0.005,0.216362 -0.008,0.324528 h 0.03 v 24.881172" stroke="transparent" stroke-width="0" fill="none"/>
+  
+  <text style="fill:red;font-size:15px;"><textPath href="#lineAC" textLength="64.9%" startOffset="-1">info@riccardozocche.it</textPath></text>
+  Sorry, your browser does not support inline SVG.
+</svg>
+
+
+
 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
