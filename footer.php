9ae24a8b99bd12131d40afc58b8c5906e746c851
giovanni <pignoni.g@gmail.com>
2024-07-29 13:00:24 +0200

    contacts

--- footer.php
+++ footer.php
@@ -14,24 +14,52 @@
 
 
 <footer id="colophon" class="site-footer compressed">
-	<hr class="separator">
-	<div class="site-info">
+	<hr class="separator hideinc">
+
+<div class="grid-x top_contacts grid-margin-x showinc">
+
+  <div class="cell small-12 medium-6 contacts_top_left">
+  	<h1>
+  		<?php echo  get_field('contacts_top_left',$post_id =123);?> 
+  	</h1>
+  </div>
+  <div class="cell small-12 medium-6 contacts_top_right ">
+  	<p><h1>
+  		<?php echo  get_field('contacts_top_right',$post_id =123)?>
+  	</h1></p>
+  </div>
+
+
+  <div style="margin-top: 20px;"class="cell small-6 large-3 small-offset-0 large-offset-6 "><h3><?php echo  get_field('contacts_bottom_left',$post_id =123);?></h3></div>
+  <div style="margin-top: 20px;" class="cell small-6 large-3"><h3><?php echo  get_field('contacts_bottom_right',$post_id =123);?></h3></div>
+
+  <div class="cell small-12 medium-12 large-6 small-offset-0 large-offset-6 newsletter_contacts" style="margin-top: 20px;">
+  	<h3><?php echo  get_field('newsletter_text',$post_id =123);?></h3>
+	 <?php  echo do_shortcode('[newsletter]');?>
+
+  </div>
+
+</div>
+
+
+
+<div class="site-info">
 
 
 	<svg id="bottom_logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 137.24 197.83"><defs><style>.cls-1{stroke-width:0px;}</style></defs><path class="cls-1" d="m96.51,129.95v-31.33l30.83,31.33h-30.83Zm-26.51,0h-22.96v-62.06h43.17v62.06h-20.21Zm37.39,61.97H29.86l38.77-53.48,38.77,53.48ZM9.9,129.95l30.83-31.33v31.33H9.9Zm12.12-35.88c-8.34,0-14.26-5.24-14.26-13.09s5.92-13.09,14.26-13.09h18.71v26.18h-18.71ZM29.98,5.82h77.28l-38.64,53.64L29.98,5.82Zm85.23,62.06h0c8.34,0,14.26,5.14,14.26,13.09s-5.92,13.09-14.26,13.09h-18.71v-26.18h18.7Zm22.03,62.06l-32.68-30.45h12.01c17.84,0,19.31-15.42,19.31-18.52,0-9.7-8.34-19-19.58-19h-42.2L114.24,6.59V0H23v6.59l40.14,55.38H20.95c-11.25,0-19.58,9.31-19.58,19,0,3.11,1.47,18.52,19.31,18.52h12.01L0,129.95v5.82h63.02l-39.95,55.47v6.59h91.12v-6.59l-39.95-55.47h63.02v-5.82Z"/></svg>
 
 
 <div class="footer_containers left">
-	<h4><?php echo  get_field('footer_text_top_left',$post_id =108);?></h4>
+	<h4 class="hideinc"><?php echo  get_field('footer_text_top_left',$post_id =108);?></h4>
 	<h5><?php echo  get_field('footer_text_bottom_left',$post_id =108);?></h5>
 </div>
 <div class="footer_containers right">
-	<h4><?php echo  get_field('footer_text_top_right',$post_id =108);?></h4>
+	<h4 class="hideinc"><?php echo  get_field('footer_text_top_right',$post_id =108);?></h4>
 	<h5><?php echo  get_field('footer_text_bottom_right',$post_id =108);?></h5>
 </div>
 
 
-<div class="footer_containers buttons">
+<div class="footer_containers buttons hideinc">
 <?php 
 $rows = get_field('footer_arch_buttons',$post_id =108);
 if( $rows ) {
@@ -73,13 +101,7 @@ if( $rows ) {
 </div>
 <div class="reveal" id="newsletterModal" data-reveal>
 
-	<h3>Iscriviti alla Newsletter di Riccardo Zocche Studio!
-		<br>
-
-	Rimani aggiornato con le ultime novità dal mondo dell'architettura! Iscriviti alla nostra newsletter per ricevere in anteprima i progetti più recenti, gli eventi esclusivi e le notizie dal nostro studio.
-	<br>
-	Approfondisci le tematiche più attuali dell'architettura.<br>
-	Non perdere l'occasione di restare connesso con il nostro studio. Inserisci la tua email e iscriviti ora!</h3>
+	<h3><?php echo  get_field('newsletter_text',$post_id =123);?></h3>
 	 <?php  echo do_shortcode('[newsletter]');?>
  
   <button class="close-button" data-close aria-label="Close modal" type="button">
