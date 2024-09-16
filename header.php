c2281962d9d8908e8e3a84361c10276f6827d572
giovanni <pignoni.g@gmail.com>
2024-09-16 13:22:48 +0200

    logo top hover

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 			
 <?php $my_home_url = apply_filters( 'wpml_home_url', get_option( 'home' ) );?>
 
-<a href="<?php echo $my_home_url; ?>">
+<a id="topLogoBtn" href="<?php echo $my_home_url; ?>">
 
 
 <svg  id="topLogo" version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
